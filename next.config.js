1ec2544dac39d438a7bec01e3eef794500eaf3ed
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-12 23:46:09 -0300

    Add img api url to nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "randomuser.me", protocol: "https" }],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
